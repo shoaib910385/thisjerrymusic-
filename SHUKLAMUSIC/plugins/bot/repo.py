c212134ecf7b8f1e823d39da5743a00b5bc8f18d
ARYAN x SHOAIB <shoaib910385@gmail.com>
2025-12-13 23:08:58 +0530

    Update repo.py

--- SHUKLAMUSIC/plugins/bot/repo.py
+++ SHUKLAMUSIC/plugins/bot/repo.py
@@ -33,7 +33,7 @@ async def start(_, msg):
     reply_markup = InlineKeyboardMarkup(buttons)
     
     await msg.reply_photo(
-        photo="https://files.catbox.moe/wifnjm.jpg",
+        photo="https://files.catbox.moe/t3zu7y.jpg",
         caption=start_txt,
         reply_markup=reply_markup
     )
